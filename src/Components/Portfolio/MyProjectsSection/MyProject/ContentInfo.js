16b0099ee2829617851b87a405b9ffae6402debe
Rostik Shafar <rostikshafar@gmail.com>
2022-03-07 17:05:00 +0200

    Int-1-new-tile-in-portfolio

--- src/Components/Portfolio/MyProjectsSection/MyProject/ContentInfo.js
+++ src/Components/Portfolio/MyProjectsSection/MyProject/ContentInfo.js
@@ -1,6 +1,10 @@
 import Home from "./Home.png";
 import Catalog from "./Catalog.png";
 import Portfolio from "./Portfolio.png";
+import motoImg1 from "./motoImg1.jpg"
+import motoImg2 from "./motoImg5.jpg"
+import motoImg3 from "./motoImg7.jpg"
+
 
 let ContentInfo = [
     {id:1, 
@@ -12,12 +16,12 @@ let ContentInfo = [
     img3: Portfolio
 },
 {id:2, 
-    firstDescription: 'I did this project at first in HTML and CSS, but then I started learning ReactJS. My first page is called "Home".On the picture you can see what it looks like.',
-    secondDescription:'As you can see here I am talking about my plans for the future.The next page is titled "Catalog".',
-    thirdDescription:'On this page I talk about my seasonal business. A slider with photos of my mopeds that I sold was added to this page.And the last page at the moment is "Portfolio".',
-    img1: Home,
-    img2: Catalog,
-    img3: Portfolio
+    firstDescription: 'My seasonal busines',
+    secondDescription:"This summer I wanted to go about my business. And then the thought came into my head why don't I buy a scooter and orchestrate.",
+    thirdDescription:"I first bought a rosebone scooter and needed spare parts. Buying parts I saw another scooter Honda dio 34 and decided to buy it too. Then I bought paint to give the mopeds a look.It turned out well.",
+    img1: motoImg1,
+    img2: motoImg2,
+    img3: motoImg3
 },
 {id:3, 
     firstDescription: 'I did this project at first in HTML and CSS, but then I started learning ReactJS. My first page is called "Home".On the picture you can see what it looks like.',
